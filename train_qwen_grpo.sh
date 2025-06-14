ca04700334fd09c2eff66110937d242ae07004ff
gemelom <3486609936@qq.com>
2025-06-15 04:42:20 +0800

    fix script and config

--- train_qwen_grpo.sh
+++ train_qwen_grpo.sh
@@ -1,4 +1,4 @@
 ACCELERATE_LOG_LEVEL=info \
     accelerate launch --config_file recipes/accelerate_configs/custom_zero3.yaml \
-    src/open_r1/grpo.py --config recipes/DeepSeek-R1-Distill-Qwen-1.5B/grpo/custom_config.yaml \
+    src/open_r1/grpo.py --config recipes/Qwen2.5-1.5B-Instruct/grpo/custom_config.yaml \
     --vllm_mode colocate
